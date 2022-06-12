ab144910af8d0625b8d9ea510965d299a8422572
gerrkoff <antonkofa@gmail.com>
2022-06-13 00:18:12 +0200

    fix extra call

--- src/js/index.js
+++ src/js/index.js
@@ -32,17 +32,6 @@ const start = async () => {
   console.log('DB starts')
 
   await insertChats(db);
-
-  try {
-    const docRef = await addDoc(collection(db, 'users'), {
-      first: 'Ada',
-      last: 'Lovelace',
-      born: 1815,
-    });
-    console.log('Document written with ID: ', docRef.id);
-  } catch (e) {
-    console.error('Error adding document: ', e);
-  }
 }
 
 render()
