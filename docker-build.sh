6470bc64c54d51a44dbf6def928dad466ef242c9
gerrkoff <antonkofa@gmail.com>
2022-11-13 23:04:48 +0100

    fix build for next

--- docker-build.sh
+++ docker-build.sh
@@ -2,7 +2,7 @@
 rm -rf build
 docker build -t smart-aggregator-web . || exit $?
 docker run -t --rm \
-    -v "$(pwd)/build":/app/build \
+    -v "$(pwd)/build":/app/.next \
     --name smart-aggregator-web-build \
     smart-aggregator-web \
     npm run build
