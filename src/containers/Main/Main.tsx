6245b2ce5717ec6865e8e5566b6607219a6e62c5
Anton Prokofiev <antonkofa@gmail.com>
2023-02-08 20:41:46 +0300

    fix styles (#17)
    * Fix
    
    * Fix posts width
    
    * Fix text in grops
    
    * Fix text in grops
    
    * lint
    
    ---------
    
    Co-authored-by: Vera Bragina <verobragina@ya.ru>

--- src/containers/Main/Main.tsx
+++ src/containers/Main/Main.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Groups, Posts, Comments } from '@containers/Main';
+import { useActivePostSelector } from '@store/activePost';
+import cn from 'classnames';
 import { withSearch, withPost, withComments } from '@/hoc-helpers';
 
 import styles from './Main.module.scss';
@@ -9,8 +11,15 @@ const PostsContainer = ({ data }) => withPost(Posts)(data);
 const CommentsContainer = ({ data }) => withComments(Comments)(data);
 
 export const Main = ({ groups, feed }) => {
+  const [wideWindow, setWideWindow] = useState<boolean>(false);
+  const { postId } = useActivePostSelector();
+
+  useEffect(() => {
+    setWideWindow(!!postId);
+  }, [postId]);
+
   return (
-    <div className={styles.main}>
+    <div className={cn(styles.main, { [styles.wide]: wideWindow })}>
       <GroupsContainer data={groups} />
       <PostsContainer data={feed} />
       <CommentsContainer data={feed} />
