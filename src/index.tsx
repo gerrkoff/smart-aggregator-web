df7859eeeeeeb9eaa867445c51f10ee8e4fa713a
Nikolay Maksimov <nikolay.maksimov.1979@gmail.com>
2023-05-25 21:43:21 +0300

    show description

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import './styles';
 
 import { createRoot } from 'react-dom/client';
+import { Helmet } from 'react-helmet';
 
 import { App } from './app';
 import { Providers } from './providers';
@@ -10,6 +11,14 @@ const roorElement = document.getElementById('root');
 if (roorElement) {
   createRoot(roorElement).render(
     <Providers>
+      <Helmet>
+        <meta
+          content="сервис получения актуальных новостей и мнений из разных источников со всего мира"
+          name="description"
+        />
+        <meta content="свежие новости, мнения, инсайты, телеграмм, телеграмм новости" name="keywords" />
+      </Helmet>
+
       <App />
     </Providers>,
   );
